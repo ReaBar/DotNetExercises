4bcf6d0b7e12d1a7609b5a570b939e1a5bd3f7bc
reabar <rea.bar@gmail.com>
2016-07-08 19:22:23 +0300

    Adding all classes

---
+++ Advanced .NET/Ex1/AttribDemo/AttribDemo/CodeReviewAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttribDemo
+{
+    [System.AttributeUsage(System.AttributeTargets.Class |
+                       System.AttributeTargets.Struct,
+                       AllowMultiple = true)]
+
+    class CodeReviewAttribute : System.Attribute
+    {
+        private string reviewrName;
+        private string reviewDate;
+        private bool codeApproved;
+
+        public CodeReviewAttribute(string name, string date, bool codeApprove)
+        {
+            reviewrName = name;
+            reviewDate = date;
+            codeApproved = codeApprove;
+        }
+
+        public string ReviewrName => reviewrName;
+        public string ReviewDate => reviewDate;
+        public bool CodeApproved => codeApproved;
+    }
+}
