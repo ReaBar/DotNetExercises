75a8c04b7d53c31b3dce136e5a2ca0bd02dea500
reabar <rea.bar@gmail.com>
2016-07-22 01:01:14 +0300

    need to solve win32exception

---
+++ Advanced .NET/Ex2/Ex2/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queries linqQueries = new Queries();
+            //linqQueries.PublicInterfaceInMscrelib();
+            linqQueries.RunningProcesses();
+        }
+    }
+}
---
+++ Advanced .NET/Ex2/Ex2/Queries.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+
+namespace Ex2
+{
+    class Queries
+    {
+        public void PublicInterfaceInMscrelib()
+        {
+            Assembly mscorlib = typeof(string).Assembly;
+
+            var interfaceQuery = mscorlib.GetTypes().Where(p => p.IsInterface).OrderBy(p => p.Name).
+                Select(p => new
+                {
+                    Name = p.Name,
+                    NumberOfMethods = p.GetMethods().Length
+                });
+
+            foreach (var interfaceFromMscorlib in interfaceQuery)
+            {
+                Console.WriteLine(interfaceFromMscorlib);
+            }
+        }
+
+        public void RunningProcesses()
+        {
+            var runningProcessesQuery =
+                Process.GetProcesses().Where(p => this.CheckProcessAccess(p) && p.Threads.Count < 5).OrderBy(p => p.Id).
+                Select(p => new
+                {
+                    ProccesName = p.ProcessName,
+                    ProcessId = p.Id,
+                    ProcessTime = p.StartTime
+                });
+
+            foreach (var runningProcess in runningProcessesQuery)
+            {
+                Console.WriteLine(runningProcess);
+            }
+        }
+
+        private bool CheckProcessAccess(Process process)
+        {
+            try
+            {
+                var result = process.ProcessName;
+            }
+
+            catch (Win32Exception e)
+            {
+                Console.WriteLine("got Win32Exception " + e.Message);
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
