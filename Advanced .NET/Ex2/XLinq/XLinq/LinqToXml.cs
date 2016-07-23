7c4cfa1dbc6ec77796e57dce18d87ddca9e01678
reabar <rea.bar@gmail.com>
2016-07-23 15:29:04 +0300

    Done.

---
+++ Advanced .NET/Ex2/XLinq/XLinq/LinqToXml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace XLinq
+{
+    class LinqToXml
+    {
+        private readonly IEnumerable<XElement> _xmlTree;
+
+        public LinqToXml(IEnumerable<XElement> xmlTree)
+        {
+            _xmlTree = xmlTree;
+        }
+
+        public void TypesWithNoProperties()
+        {
+            var query = _xmlTree.Where(t =>
+            {
+                var xElement = t.Element("Properties");
+                return xElement != null && !xElement.Descendants().Any();
+            })
+                .OrderBy(t => (string)t.Attribute("FullName"))
+                .Select(t => (string)t.Attribute("FullName"));
+
+            var xAttributes = query as string[] ?? query.ToArray();
+            foreach (var xAttribute in xAttributes)
+            {
+                Console.WriteLine($"{xAttribute}");
+            }
+
+            Console.WriteLine($"Number of types without properties: {xAttributes.Count()}");
+        }
+
+        public void SumOfMethods()
+        {
+            var query = _xmlTree.Select(m => m.Element("Methods").Descendants().Count()).Sum();
+            Console.WriteLine($"Sum of methods: {query}");
+        }
+
+        public void SumOfProperties()
+        {
+            var query = _xmlTree.Select(p => p.Element("Properties").Descendants().Count()).Sum();
+            Console.WriteLine($"Sum of properties: {query}");
+        }
+
+        public void MostCommonTypeAsParameter()
+        {
+            var query = _xmlTree.Descendants("Parameter")
+                .GroupBy(t => (string) t.Attribute("Type"))
+                .Select(g => new
+                {
+                    ParameterType = g.Key,
+                    ParameterCount = g.Count()
+                }).OrderByDescending(p => p.ParameterCount).First();
+
+            Console.WriteLine($"Most common type as parameter: {query}");
+        }
+
+        public void SortTypesByNumOfMethods()
+        {
+            var query = _xmlTree.OrderByDescending(m => m.Descendants("Method").Count())
+                .Select(m => new
+                {
+                    TypeName = (string)m.Attribute("FullName"),
+                    MethodsCount = m.Descendants("Method").Count(),
+                    PropertyCount = m.Descendants("Property").Count()
+                });
+
+            var newXmlTree = query.Select(t => new XElement("Type", new XAttribute("FullName", t.TypeName),
+                new XAttribute("MethodCount", t.MethodsCount),
+                new XAttribute("PropertyCount", t.PropertyCount)));
+
+            XDocument doc = new XDocument();
+            doc.Add(new XElement("XML", newXmlTree));
+            doc.Save("3_d_XMLTree_" + DateTime.Now.ToString("dd-MM-yyyy") + ".XML");
+        }
+
+        public void GroupAllTypesByNumOfMethods()
+        {
+            var query = _xmlTree.OrderBy(t => (string)t.Attribute("FullName"))
+                .GroupBy(t => new
+                {
+                    TypeName = (string) t.Attribute("FullName"),
+                    MethodCount = t.Descendants("Method").Count()
+                }).OrderByDescending(g => g.Key.MethodCount)
+                .Select(g => new
+                {
+                    g.Key.TypeName, g.Key.MethodCount
+                });
+            
+            foreach (var item in query)
+            {
+                Console.WriteLine(item.TypeName + ", " + item.MethodCount);
+            }
+        }
+    }
+}
--- Advanced .NET/Ex2/XLinq/XLinq/Program.cs
+++ Advanced .NET/Ex2/XLinq/XLinq/Program.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using System.Xml.Linq;
 
 namespace XLinq
@@ -12,10 +9,28 @@ namespace XLinq
     {
         static void Main(string[] args)
         {
-            var linqToXml = Assembly.GetAssembly(typeof(string)).GetExportedTypes().Where(p => p.IsClass)
+
+            var xmlTree = Assembly.GetAssembly(typeof(string)).GetExportedTypes().Where(p => p.IsClass && p.IsPublic)
                 .Select(t => new XElement("Type", new XAttribute("FullName", t.FullName),
                     new XElement("Properties", t.GetProperties(BindingFlags.Instance | BindingFlags.Public)
-                        .Select(p => new XElement("Property", new XAttribute("Name", p.Name), new XAttribute("Type", p.PropertyType.Name))))));
+                        .Select(p => new XElement("Property", new XAttribute("Name", p.Name), new XAttribute("Type", p.PropertyType.Name)))),
+                    new XElement("Methods", t.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly)
+                        .Select(m => new XElement("Method", new XAttribute("Name", m.Name), new XAttribute("ReturnType", m.ReturnType),
+                            new XElement("Parameters", m.GetParameters()
+                                .Select(p => new XElement("Parameter", new XAttribute("Name", p.Name), new XAttribute("Type", p.ParameterType.Name)))))))));
+
+            XDocument doc = new XDocument();
+            doc.Add(new XElement("XML", xmlTree));
+            doc.Save("XMLTree_" + DateTime.Now.ToString("dd-MM-yyyy") + ".XML");
+
+            LinqToXml linqToXml = new LinqToXml(xmlTree);
+            linqToXml.TypesWithNoProperties();
+            linqToXml.SumOfMethods();
+            linqToXml.SumOfProperties();
+            linqToXml.MostCommonTypeAsParameter();
+            linqToXml.SortTypesByNumOfMethods();
+            linqToXml.GroupAllTypesByNumOfMethods();
+            
         }
     }
 }
