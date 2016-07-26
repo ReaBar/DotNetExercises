aa11d148ed23e2253bc6c97a2b9d6cd365cec007
reabar <rea.bar@gmail.com>
2016-07-26 23:47:29 +0300

    Primes Calculator - Basic Multithreading

---
+++ Advanced .NET/Ex3/PrimesCalculator/PrimesCalculator/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrimesCalculator
+{
+    public partial class Form1 : Form
+    {
+        private int _minValue;
+        private int _maxValue;
+        private readonly PrimeNumbers _primeNumbers;
+
+        public Form1()
+        {
+            InitializeComponent();
+            label3.Text = "From:";
+            label2.Text = "To:";
+            label1.Text = "Primes Calculator";
+            button1.Text = "Calculate";
+            _primeNumbers = new PrimeNumbers();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBox2.Text, out _minValue) && int.TryParse(textBox1.Text,out _maxValue) && _minValue >= 2 && _minValue <= _maxValue)
+            {
+                if (listBox1.Items.Count != 0)
+                {
+                    listBox1.Items.Clear();
+                }
+
+                List<string> primesList = await Task.Run(() => _primeNumbers.CalcPrimes(_minValue, _maxValue));
+                listBox1.BeginUpdate();
+                foreach (var prime in primesList)
+                {
+                    listBox1.Items.Add(prime);
+                }
+                listBox1.EndUpdate();
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Advanced .NET/Ex3/PrimesCalculator/PrimesCalculator/PrimeNumbers.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+namespace PrimesCalculator
+{
+    class PrimeNumbers
+    {
+        public List<string> CalcPrimes(int subtracted, int subtrahend)
+        {
+            List<string> primesList = new List<string>();;
+
+            for (int i = subtracted; i <= subtrahend; i++)
+            {
+                bool isPrime = IsNumPrime(i);
+                if (isPrime)
+                {
+                    primesList.Add(i.ToString());
+                }
+            }
+
+            return primesList;
+        }
+
+        private bool IsNumPrime(int num)
+        {
+            if (num == 2 || num == 3)
+            {
+                return true;
+            }
+
+            if ((num % 2 == 0) || (num % 3 == 0))
+            {
+                return false;
+            }
+
+            for (int i = 5; i * i <= num; i += 6)
+            {
+                if (num % i == 0 || num % (i + 2) == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+
+        }
+    }
+}
