61c96e06758d77172b00568a10d86a22565f89a1
reabar <rea.bar@gmail.com>
2016-07-27 12:10:55 +0300

    Cancelling a Long Running Task completed

---
+++ Advanced .NET/Ex3/PrimesCalculator2/PrimesCalculator/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrimesCalculator
+{
+    public partial class Form1 : Form
+    {
+        private uint _minValue;
+        private uint _maxValue;
+        private readonly PrimeNumbers _primeNumbers;
+        private CancellationTokenSource _cancelTokenSource;
+        public Form1()
+        {
+            InitializeComponent();
+            label3.Text = "From:";
+            label2.Text = "To:";
+            label1.Text = "Primes Calculator";
+            button1.Text = "Calculate";
+            button2.Text = "Cancel";
+            _primeNumbers = new PrimeNumbers();
+            _cancelTokenSource = new CancellationTokenSource();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            if (uint.TryParse(textBox2.Text, out _minValue) && uint.TryParse(textBox1.Text, out _maxValue) &&
+                _minValue >= 2 && _minValue <= _maxValue)
+            {
+                if (listBox1.Items.Count != 0)
+                {
+                    listBox1.Items.Clear();
+                }
+
+                List<string> primesList = await Task.Run(() => _primeNumbers.CalcPrimes(_minValue, _maxValue,_cancelTokenSource.Token), _cancelTokenSource.Token);
+
+                if (primesList == null)
+                {
+                    MessageBox.Show("Proccess was canceled");
+                }
+                else
+                {
+                    listBox1.BeginUpdate();
+                    foreach (var prime in primesList)
+                    {
+                        listBox1.Items.Add(prime);
+                    }
+                    listBox1.EndUpdate();
+                }
+
+                _cancelTokenSource.Dispose();
+                _cancelTokenSource = new CancellationTokenSource();
+            }
+
+            else
+            {
+                MessageBox.Show("Invalid Input!");
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            _cancelTokenSource.Cancel();
+        }
+    }
+}
---
+++ Advanced .NET/Ex3/PrimesCalculator2/PrimesCalculator/PrimeNumbers.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+
+namespace PrimesCalculator
+{
+    class PrimeNumbers
+    {
+        public List<string> CalcPrimes(uint subtracted, uint subtrahend, CancellationToken cancellationToken)
+        {
+            List<string> primesList = new List<string>();;
+
+            for (uint i = subtracted; i <= subtrahend; i++)
+            {
+                bool isPrime = IsNumPrime(i);
+
+                if (cancellationToken.IsCancellationRequested)
+                {
+                    return null;
+                }
+
+                if (isPrime)
+                {
+                    primesList.Add(i.ToString());
+                }
+            }
+
+            return primesList;
+        }
+
+        private bool IsNumPrime(uint num)
+        {
+            if (num == 2 || num == 3)
+            {
+                return true;
+            }
+
+            if ((num % 2 == 0) || (num % 3 == 0))
+            {
+                return false;
+            }
+
+            for (int i = 5; i * i <= num; i += 6)
+            {
+                if (num % i == 0 || num % (i + 2) == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+
+        }
+    }
+}
