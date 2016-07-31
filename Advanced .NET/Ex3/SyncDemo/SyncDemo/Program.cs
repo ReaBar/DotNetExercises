23b24daa3fc1329f848dfed48e6d171707cf4b01
reabar <rea.bar@gmail.com>
2016-07-31 20:22:06 +0300

    SyncDemo project - Mutex

---
+++ Advanced .NET/Ex3/SyncDemo/SyncDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SyncDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool _mutexState;
+            Mutex _mutex = new Mutex(true, "SyncFileMutex", out _mutexState);
+
+
+        }
+    }
+}
+
